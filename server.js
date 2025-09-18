86b486714b41e3da8c6970977382789d01f340c4
Gilzilene20 <gos20@aluno.ifnmg.edu.br>
2025-09-18 18:18:46 +0000

    Atividade 16

--- server.js
+++ server.js
@@ -13,14 +13,25 @@ const { Pool } = pkg;    // Utiliza a Classe Pool do Postgres
 
 app.get("/", async (req, res) => {        // Cria endpoint na rota da raiz do projeto
   console.log("Rota GET / solicitada");
-  dotenv.config();         // Carrega e processa o arquivo .env
-const { Pool } = pkg;    // Utiliza a Classe Pool do Postgres
 
+const db = new Pool({  
+  connectionString: process.env.URL_BD,
+});
+
+let dbStatus = "ok";
+try {
+  await db.query("SELECT 1");
+} catch (e) {
+  dbStatus = e.message;
+}
 
   res.json({
 		message: "API para _____",      // Substitua pelo conteúdo da sua API
     author: "Gilzilene Orneles de Sales",    // Substitua pelo seu nome
     statusBD: dbStatus   // Acrescente esta linha
-
+})
   });
+
+app.listen(port, () => {            // Um socket para "escutar" as requisições
+  console.log(`Serviço rodando na porta:  ${port}`);
 });
