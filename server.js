6a76984fe5f9b82be9a2787f329a5bff8f09f441
Gilzilene20 <gos20@aluno.ifnmg.edu.br>
2025-09-18 18:05:26 +0000

    OK

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+
+
 import express from "express";      // Requisição do pacote do express
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
-app.get("/", (req, res) => {        // Cria endpoint na rota da raiz do projeto
+
+dotenv.config();         // Carrega e processa o arquivo .env
+const { Pool } = pkg;    // Utiliza a Classe Pool do Postgres
+
+
+app.get("/", async (req, res) => {        // Cria endpoint na rota da raiz do projeto
   console.log("Rota GET / solicitada");
+  dotenv.config();         // Carrega e processa o arquivo .env
+const { Pool } = pkg;    // Utiliza a Classe Pool do Postgres
+
+
   res.json({
 		message: "API para _____",      // Substitua pelo conteúdo da sua API
-    author: "Seu_nome_completo",    // Substitua pelo seu nome
-  });
-});
+    author: "Gilzilene Orneles de Sales",    // Substitua pelo seu nome
+    statusBD: dbStatus   // Acrescente esta linha
 
-app.listen(port, () => {            // Um socket para "escutar" as requisições
-  console.log(`Serviço rodando na porta:  ${port}`);
+  });
 });
